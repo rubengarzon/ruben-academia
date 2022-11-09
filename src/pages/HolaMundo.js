1ee7ab9190552c39ede6ed5f5cdf6c460f9dfa90
Rubén Garzón <rubengarzonzafra@gmail.com>
2022-11-09 11:41:26 +0100

    seccion holamundo añadido

--- src/pages/HolaMundo.js
+++ src/pages/HolaMundo.js
@@ -1,6 +1,16 @@
 import "../styles/holaMundo.css";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { dracula } from "react-syntax-highlighter/dist/esm/styles/prism";
 
 const HolaMundo = () => {
+  const codeString = `<html lang="en">
+  <head>
+    <script src="script.js"></script>
+  </head>
+  <body>
+    <h1>Hola Mundo</h1>
+  </body>
+</html> `;
   return (
     <>
       <div className="link">
@@ -9,9 +19,34 @@ const HolaMundo = () => {
       <div>
         <h1>Hola Mundo</h1>
       </div>
-      <span className="objective">
-        Objetivo: Mostrar una alerta que diga "Hola Mundo"
-      </span>
+      <div className="block">
+        <p className="content">
+          Creamos un archivo html y dentro de él escribimos la etiqueta{" "}
+          <strong>script</strong>, la etiqueta script tiene un atributo llamado{" "}
+          <strong>src</strong> que es donde le indicamos la ruta del archivo
+          javascript que queremos ejecutar.
+        </p>
+        <div className="code">
+          <SyntaxHighlighter language="html" style={dracula}>
+            {codeString}
+          </SyntaxHighlighter>
+        </div>
+      </div>
+      <div className="block">
+        <p className="content">
+          Utilizamos el método <strong>alert</strong> que muestra un mensaje en
+          una ventana emergente.
+        </p>
+        <div className="code">
+          <SyntaxHighlighter language="javascript" style={dracula}>
+            {`alert("Hola Mundo");`}
+          </SyntaxHighlighter>
+        </div>
+      </div>
+      <div className="block">
+        <p className="content">El resultado sería este:</p>
+        <img src="https://i.ibb.co/vsmpD2Q/holamundo.png" alt="Hola Mundo" />
+      </div>
     </>
   );
 };
